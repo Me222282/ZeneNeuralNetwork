cb6ad1605e085793d410f664ad25eede22c5c032
Zene <oinkymail3@gmail.com>
2023-04-17 18:49:53 +0100

    Inital Commit
    NeuralLayer.cs is the basic inter-node scalers.

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Zene Neural Networking
+
+Basic neural networking system for training AI.
+
+## Credits
+
+None
---
+++ src/NeuralLayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Zene.Structs;
+
+namespace Zene.NeuralNetwork
+{
+    public class NeuralLayer : IMatrix
+    {
+        public NeuralLayer(int preSize, int postSize)
+        {
+            Rows = postSize;
+            Columns = preSize;
+            _matrix = new double[postSize * preSize];
+        }
+
+        public int Rows { get; }
+        public int Columns { get; }
+
+        private readonly double[] _matrix;
+        public int Length => _matrix.Length;
+
+        public double this[int x, int y]
+        {
+            get => _matrix[x + (y * Rows)];
+            set => _matrix[x + (y * Rows)] = value;
+        }
+        public double this[int index]
+        {
+            get => _matrix[index];
+            set => _matrix[index] = value;
+        }
+        public double this[Index index]
+        {
+            get => _matrix[index.Value];
+            set => _matrix[index.Value] = value;
+        }
+
+        public MatrixSpan MatrixData() => new MatrixSpan(Rows, Columns, _matrix);
+    }
+}
